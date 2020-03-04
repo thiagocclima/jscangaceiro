2b7a5d493d7141bb85178acd97990cbd80c605bc
Thiago <thiago.constantino@digivox.com.br>
2020-03-04 17:49:34 -0300

    zera campos manualmente

--- client/app/index.js
+++ client/app/index.js
@@ -21,10 +21,13 @@ document.querySelector('.form').addEventListener('submit', function(event){
     });
 
     var tdVolume = document.createElement('td');
-
     tdVolume.textContent = campos[1].value * campos[2].value;
-
     tr.appendChild(tdVolume);
     
     tbody.appendChild(tr);
+
+    campos[0].value = '';
+    campos[1].value = 1;
+    campos[2].value = 0;
+    campos[0].focus();
 });
